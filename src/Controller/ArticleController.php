ed492ed10ba81b66326975a8146e79a9e738ec26
Martin Komischke <martin.komischke@quentic.com>
2020-03-31 12:02:24 +0200

    use named route so that it matches with './bin/console debug:router'

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {

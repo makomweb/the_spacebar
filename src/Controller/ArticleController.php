a73979b186a72fa6ae21d61826dafdd2073e94e6
Martin Komischke <martin.komischke@quentic.com>
2020-03-31 12:43:19 +0200

    fix heart link and forward slug

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -33,6 +33,7 @@ class ArticleController extends AbstractController
 
         return $this->render('article/show.html.twig', [
             'title' => ucwords((str_replace('-', ' ', $slug))),
+            'slug' => $slug,
             'comments' => $comments
         ]);
     }

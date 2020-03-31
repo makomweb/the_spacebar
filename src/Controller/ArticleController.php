c978dee282d5e789f21c96fd016fb1257b5bd257
Martin Komischke <martin.komischke@quentic.com>
2020-03-31 10:22:52 +0200

    finish using Route annotation

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,10 +4,14 @@
 namespace App\Controller;
 
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage()
     {
         return new Response('OMG! My first page already! Whoop!');
